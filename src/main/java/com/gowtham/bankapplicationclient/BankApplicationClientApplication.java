12b48ef7029d1c3a0bb8995c1cbba2cc0ca4b57d
gowthamparamasivam <gowthamparamasivam66@gmail.com>
2020-04-14 12:13:06 +0530

    Initial commit

---
+++ src/main/java/com/gowtham/bankapplicationclient/BankApplicationClientApplication.java
@@ -0,0 +1,13 @@
+package com.gowtham.bankapplicationclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApplicationClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gowtham/bankapplicationclient/BankApplicationClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gowtham.bankapplicationclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
